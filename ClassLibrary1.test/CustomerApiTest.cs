e7033c45389eb71e43d78dc2c83cb31202d7e8bd
Todd Miranda <todd@nxtdimension.com>
2016-10-26 17:48:24 -0500

    Modified test project to use correct template and write a test

---
+++ ClassLibrary1.test/CustomerApiTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.test
+{
+    [TestFixture]
+    public class CustomerApiTest
+    {
+        [Test]
+        public void ShouldReturnNumberOfValuesEqualToUpperBound()
+        {
+            ICustomerApi sut = new CustomerApi();
+            var configDictionary = new Dictionary<int, string>();
+            int expectedNumberOfValues = sut.GetNumbers(100, configDictionary).Count();
+            Assert.That(expectedNumberOfValues, Is.EqualTo(100));
+        }
+    }
+}
