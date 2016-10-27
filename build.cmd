05b3080aee0f98073382c85b0c59a174e22fdc9d
Todd Miranda <todd@nxtdimension.com>
2016-10-26 19:37:58 -0500

    Added a minimal build script

---
+++ build.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+if exist %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+if exist "%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
+
+"%MSBUILD%" build\build.proj %*
